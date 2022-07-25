424dfc1ef3a18bba2537b6a452f3153016e112a8
Corentin Jabot <corentinjabot@gmail.com>
2022-07-25 15:20:35 +0200

    Remove old implementation

--- tools/casing.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "cedilla/generated_props_casing.hpp"
-#include "cedilla/casing_impl.hpp"
-
--- tools/properties.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-//#include "cedilla/details/generated_props.hpp"
-//#include "cedilla/properties_impl.hpp"
-//#include "cedilla/regex.h"
-
